c80d0ea2e9d3544341fd402f88c8c55559e2ae33
ArnoldoCortez <79164634+ArnoldoCortez@users.noreply.github.com>
2023-09-19 00:27:23 -0600

    netlify adapter added to enable SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://heartfelt-platypus-333417.netlify.app/",
+  output: "server",
+  adapter: netlify(),
 });
