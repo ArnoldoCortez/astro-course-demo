527c2e24c1d7de542929da58a35279d20c2b8a6d
ArnoldoCortez <79164634+ArnoldoCortez@users.noreply.github.com>
2023-09-19 21:17:40 -0600

    add post per page env variable

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  POSTS_PER_PAGE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
