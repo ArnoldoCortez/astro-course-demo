ac659d8197fd9bd0b4066f9fa39ba7b0aab58eaa
ArnoldoCortez <79164634+ArnoldoCortez@users.noreply.github.com>
2023-09-21 01:03:52 -0600

    create success and failure pages

--- src/pages/api/newsletter.ts
+++ src/pages/api/newsletter.ts
@@ -7,12 +7,23 @@ export const POST: APIRoute = async ({ request, redirect }) => {
   const email = formData.get("email") as string;
 
   if (!email) {
-    return redirect("/newsletter/failure", 307);
+    return redirect("/newsletter/failure?message=Email is required", 307);
   }
 
-  await client.db.subscribers.create({
-    email,
-  });
+  try {
+    await client.db.subscribers.create({
+      email,
+    });
 
-  return redirect("/newsletter/success", 307);
+    return redirect("/newsletter/success", 307);
+  } catch (error: any) {
+    if (error?.errors[0]?.status === 400) {
+      return redirect(
+        "/newsletter/failure?message=You are already subscribed",
+        307,
+      );
+    }
+
+    return redirect("/newsletter/failure?message=Something went wrong", 307);
+  }
 };
